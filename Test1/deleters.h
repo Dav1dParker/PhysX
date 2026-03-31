97cf61d5d764cd9450c2e95403f36452305102fd
Andrey <smail2204973@gmail.com>
2026-03-31 17:58:42 +0300

    Create PhysX, PVD, scene, dispatcher

--- Test1/deleters.h
+++ Test1/deleters.h
@@ -1,5 +1,8 @@
+#pragma once
+
 #include <PxPhysicsAPI.h>
 
+#include <memory>
 
 struct PxFoundationDeleter {
     void operator()(physx::PxFoundation* p) const noexcept {
@@ -12,7 +15,6 @@ struct PxPhysicsDeleter {
         if (p) p->release();
     }
 };
-
 
 struct PxSceneDeleter {
     void operator()(physx::PxScene* p) const noexcept {
@@ -61,4 +63,14 @@ struct PxRigidDynamicDeleter {
         if (p) p->release();
     }
 };
-#pragma once
+
+using PxFoundationPtr = std::unique_ptr<physx::PxFoundation, PxFoundationDeleter>;
+using PxPhysicsPtr = std::unique_ptr<physx::PxPhysics, PxPhysicsDeleter>;
+using PxScenePtr = std::unique_ptr<physx::PxScene, PxSceneDeleter>;
+using PxPvdPtr = std::unique_ptr<physx::PxPvd, PxPvdDeleter>;
+using PxPvdTransportPtr = std::unique_ptr<physx::PxPvdTransport, PxPvdTransportDeleter>;
+using PxDefaultCpuDispatcherPtr = std::unique_ptr<physx::PxDefaultCpuDispatcher, PxDefaultCpuDispatcherDeleter>;
+using PxMaterialPtr = std::unique_ptr<physx::PxMaterial, PxMaterialDeleter>;
+using PxRigidStaticPtr = std::unique_ptr<physx::PxRigidStatic, PxRigidStaticDeleter>;
+using PxShapePtr = std::unique_ptr<physx::PxShape, PxShapeDeleter>;
+using PxRigidDynamicPtr = std::unique_ptr<physx::PxRigidDynamic, PxRigidDynamicDeleter>;
